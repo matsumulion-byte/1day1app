9b6676663cfe495d77375cfdfab44362f157f200
matsumulion-byte <matsumulion@gmail.com>
2025-10-01 02:16:33 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      { source: '/:date(\\d{4}-\\d{2}-\\d{2})', destination: '/:date/', permanent: false },
+    ];
+  },
   async rewrites() {
     return [
-      // /2025-10-01 → /apps/2025-10-01/index.html
-      { source: '/:date(\\d{4}-\\d{2}-\\d{2})', destination: '/apps/:date/index.html' },
-      // 末尾スラッシュでも対応（/2025-10-01/）
       { source: '/:date(\\d{4}-\\d{2}-\\d{2})/', destination: '/apps/:date/index.html' },
     ];
   },
