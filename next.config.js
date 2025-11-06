c3463d3e6102e9b1863982a4e82cb5a442191a6f
hironorimatsumura <matsumulion@gmail.com>
2025-11-06 23:40:48 +0900

    fix(rewrite): handle trailing slash for /YYYY-MM-DD to serve app index

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
     return [
       // /YYYY-MM-DD → public/apps/YYYY-MM-DD/index.html
       { source: '/:date(\\d{4}-\\d{2}-\\d{2})', destination: '/apps/:date/index.html' },
+      // /YYYY-MM-DD/ → public/apps/YYYY-MM-DD/index.html (trailing slash)
+      { source: '/:date(\\d{4}-\\d{2}-\\d{2})/', destination: '/apps/:date/index.html' },
 
       // /YYYY-MM-DD/... → public/apps/YYYY-MM-DD/... （画像やJS、CSSも通す）
       { source: '/:date(\\d{4}-\\d{2}-\\d{2})/:path*', destination: '/apps/:date/:path*' },
